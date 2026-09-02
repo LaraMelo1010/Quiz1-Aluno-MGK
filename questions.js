abe411842d0a04b475c4a0149b04b675218653dc
laramsrosa <lara.melo.102013@gmail.com>
2026-09-02 16:29:09 -0300

    script.js and list

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        number: 1,
+        question: "What ability programming desenvolves?",
+        answer: "problems solucion, creativity, logical thinking",
+        options: [
+            "comunication abiliity",
+            "just logical thinking",
+            "problems solucion, creativity, logical thinking",
+            "game design"
+        ]
+
+    },
+    {
+        number: 2,
+        question: "What is a variable used for in programming?",
+        answer: "To store data",
+        options: [
+            "To delete a program",
+            "To store data",
+            "To turn off a computer",
+            "To create a keyboard"
+        ]
+    },
+    {
+        number: 3,
+        question: "What does HTML stand for?",
+        answer: "HyperText Markup Language",
+        options: [
+            "HyperText Markup Language",
+            "HighText Machine Language",
+            "HyperTool Markup Language",
+            "Home Tool Markup Language"
+        ]
+    },
+    {
+        number: 4,
+        question: "Which of these is a programming language?",
+        answer: "Python",
+        options: [
+            "Google",
+            "Windows",
+            "Python",
+            "Chrome"
+        ]
+    },
+    {
+        number: 5,
+        question: "What is a loop used for in programming?",
+        answer: "To repeat instructions",
+        options: [
+            "To repeat instructions",
+            "To delete variables",
+            "To change the computer's hardware",
+            "To open a website"
+        ]
+    }
+]
---
+++ script.js
@@ -0,0 +1,17 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box ");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const time_text = document.querySelector(".timer .time_left_txt");
+const time_count = document.querySelector(".timer .timer_sec");
+
+ start_btn.onclick =  () => {
+    info_box.classList.add("activeInfo");
+ }
+ exit_btn.onclick =  () => {
+    info_box.classList.remove("activeInfo");
+ }
