fd8b52794ab87ca10bbb2b1779769aa0965c5399
laramsrosa <lara.melo.102013@gmail.com>
2026-09-03 16:15:00 -0300

    Update of restart btn and quit btn

--- script.js
+++ script.js
@@ -8,10 +8,49 @@ const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector("header .time_line");
 const time_text = document.querySelector(".timer .time_left_txt");
 const time_count = document.querySelector(".timer .timer_sec");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+ let timeValue = 15;
+ let que_number = 1;
+ let que_count = 0;
+ let userScore = 0;
+ let widthValue = 0;
+ let counter;
+ let couterLine;
 
  start_btn.onclick =  () => {
     info_box.classList.add("activeInfo");
  }
  exit_btn.onclick =  () => {
     info_box.classList.remove("activeInfo");
+ }
+
+ continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+    showQuestions(0);
+    queCounter(1);
+    startTimer(15);
+    startTimerLine(0);
+ }
+ restart_quiz.onclick = () => {
+    quiz_box.classList.add("activeQuiz");
+    result_box.classList.remove("activeResult");
+    timeValue = 15;
+    que_count = 0;
+    que_number = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuestions(que_count);
+    queCounter(que_number);
+    clearInterval(counter);
+    clearInterval(couterLine);
+    startTimer(timeValue);
+    startTimerLine(widthValue);
+    time_text.textContent = "Tempo restante: ";
+    next_btn.classList.remove("show");
+ }
+ quit_quiz.onclick = () => {
+    window.location.reload();
  }
