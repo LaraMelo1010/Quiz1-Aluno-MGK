5239ff06accc5341b831e93fd4d8365779e66c7d
laramsrosa <lara.melo.102013@gmail.com>
2026-09-17 16:30:24 -0300

    functions

--- script.js
+++ script.js
@@ -93,7 +93,13 @@ const botton_ques_counter = document.querySelector("footer .total_q");
         option[i].setAttribute("onclick","optionSelected(this)")
     }
  }
+
+ let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+ let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
  function optionSelected (answer) {
+   clearInterval(counter);
+   clearInterval(couterLine);
    let userAns = answer.textContent;
    let correctAns = questions[que_count].answer;
    const allOptions = option_list.children.length;
@@ -122,4 +128,64 @@ const botton_ques_counter = document.querySelector("footer .total_q");
       option_list.children[i].classList.add("disabled");
    }
    next_btn.classList.add("show");
+ }
+ function showResult () {
+   info_box.classList.remove("activeInfo");
+   quiz_box.classList.remove("äctiveQuiz");
+   result_box.classList.add("activeResult");
+   const scoreText = result_box.querySelector(".score_text");
+   if (userScore > 4) {
+      let scoreTag = '<span>Congratulations! , Your score is: <p>' + userScore + '</p> de <p>' + questions.length + '</p></span>';
+      scoreText.innerHTML = scoreTag;
+   }
+   else if (userScore > 2) {
+      let scoreTag = '<span>Great , Your score is: <p>' + userScore + '</p> de <p>' + questions.length + '</p></span>';
+      scoreText.innerHTML = scoreTag;
+   }
+   else {
+      let scoreTag = '<span>You failed , Your score is: <p>' + userScore + '</p> de <p>' + questions.length + '</p></span>';
+      scoreText.innerHTML = scoreTag;
+   }
+ }
+ function startTime (time) {
+   counter = setInterval(timer, 1000);
+   function timer () {
+      time_count.textContent = time;
+      time--;
+      if (time < 9) {
+         let addZero = time_count.textContent;
+         time_count.textContent = "0" + addZero;
+      }
+      if (time < 0) {
+         clearInterval(counter);
+         time_text.textContent = "You dont have more time";
+         const allOptions = option_list.children.length;
+         let correctAns = questions[que_count].answer;
+         for(i = 0; i < allOptions; i++) {
+            if(option_list.children[i].textContent==correctAns){
+            option_list.children[i].setAttribute("class", "optionCorrect");
+            option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+            console.log("correct answer selected");
+            }
+         }
+         for(i = 0; i < allOptions; i++) {
+            option_list.children[i].classList.add("disabled");
+         }
+         next_btn.classList.add("show");
+      }
+   }
+ }
+ function startTimeLine () {
+   couterLine = setInterval(timer,29);
+   function timer () {
+      time += 1;
+      time_line.style.width = time + "px";
+      if (time > 549) {
+         clearInterval(couterLine);
+      }
+   }
+ }
+ function queCounter (index) {
+   let totalQcounterTag = '<span><p>' + index + '</p> de <p>' + questions.length + '</p> quetions </span>';
+   botton_ques_counter.innerHTML = totalQcounterTag; 
  }
